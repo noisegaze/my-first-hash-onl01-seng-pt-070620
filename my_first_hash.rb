f9393224a1ba2f0e19eb52c31d562204e0d2ebe8
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-16 23:21:42 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,12 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {"name"=>"Ben","age"=>39}
 
   
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+ the_manifest = {"whale bone corsets"=>5,"porcelain vases"=>2,"oil paintings"=>3}
 
 
 end
@@ -19,9 +17,10 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["value"]
 end
 
 def adding
@@ -32,12 +31,14 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"]=2 
 
   # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["gun powder"]=4
 
 
 
   # return the shipping_manifest hash below
 
 end
+shipping_manifest
